5ab93e0e642f91f4ff41d4a901fa25d8dea64c6c
mohamedanas84 <30699810+mohamedanas84@users.noreply.github.com>
2017-08-27 14:53:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ function createTemplate (data) {
                                 </div>
                             </div>
                         </body>
-                    </html>`;
+                    </html>`
     return htmlTemplate;
 }
 

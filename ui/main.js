3f039c2c91ac998f55c564c07e11f61f81744982
mohamedanas84 <30699810+mohamedanas84@users.noreply.github.com>
2017-08-27 16:14:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
+var img = document.getElementById('modi');
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + "px";
+}
+img.onclick = function () {
+    var interval = setInterval(moveRight,100);
+};
+

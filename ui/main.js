ece35fe8378aa4e864df93f0c3bed16db17fb0b6
mohamedanas84 <30699810+mohamedanas84@users.noreply.github.com>
2017-08-27 16:16:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
 var img = document.getElementById('modi');
+var marginLeft = 0; 
 function moveRight() {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + "px";

eba29fca94b939aa6830dfd281a16122a8ff8dbd
mohamedanas84 <30699810+mohamedanas84@users.noreply.github.com>
2017-08-28 22:04:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var img = document.getElementById('modi');
 var marginLeft = 0; 
 function moveRight() {
@@ -7,5 +7,12 @@ function moveRight() {
 }
 img.onclick = function () {
     var interval = setInterval(moveRight,30);
+};*/
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
 

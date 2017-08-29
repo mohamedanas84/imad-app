4b5442b26c581ac57898fbde4f9f1b2d7422e8c8
mohamedanas84 <30699810+mohamedanas84@users.noreply.github.com>
2017-08-29 21:49:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,9 +19,16 @@ button.onclick = function() {
     request.onreadystatechange = function() {
        if(request.readyState == XMLHttpRequest.DONE){
            if(request.status == 200){
-              var counter = request.responseText;
+             /* var counter = request.responseText;
               var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
+              span.innerHTML = counter.toString();*/
+                var names = ['name1','name2','name3','name4'];
+                var list = '';
+                for (var i=0; i < names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
           }
        }
     };
@@ -35,13 +42,26 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i=0; i < names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
+    //Create a request object.
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store into the variable
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE){
+             if(request.status == 200){
+                var names = ['name1','name2','name3','name4'];
+                var list = '';
+                for (var i=0; i < names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+             }
+        }
+    };
+    request.open('GET','http://imohamedanas.imad.hasura-app.io/submit-name?name=' + name,true)
+    request.send(null);
 };
 
 
